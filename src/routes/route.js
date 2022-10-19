251a06460f21a371063726b29e6bc01c3c9892c1
Aadarshpandit21 <anshulkanhaiyyapandit@gmail.com>
2022-10-19 23:10:40 +0530

    missingNumber

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,67 +2,21 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-let players = [
-    {
-        "name": "manish",
-        "dob": "1/1/1995",
-        "gender": "male",
-        "city": "jalandhar",
-        "sports": [
-        "swimming"
-        ]
-        },
-        {
-            "name": "aadarsh",
-            "dob": "21/07/2000",
-            "gender": "male",
-            "city": "chhindwara",
-            "sports": [
-            "Chess"
-            ]
-            }
-          ,
-          {
-            "name": "Akshay",
-            "dob": "6/07/1998",
-            "gender": "male",
-            "city": "nagpur",
-            "sports": [
-            "Boxing"
-            ]
-            }
-              ,{
-                "name": "bobby",
-                "dob": "4/12/1997",
-                "gender": "male",
-                "city": "indore",
-                "sports": [
-                "drawing"
-                ]
-                }                
-]
 
-router.post('/test-me', function (req, res){
-    let obj={};
-  let element= req.body.obj;
-  flag=1;
-for(let i=0;i<players.length;i++){
-    if(players[i].name===element.name){
-        flag=0;
-        break;
-    }
-}
-if(flag==1){
-    players.push(element);
-}
-//console.log(players); 
- //players.push(element);
-res.send(players)
+router.get("/sol1", function (req, res) {
+  
+    let arr= [1,2,3,5,6,7]
+     let n = arr.length;
+     let missingNumber = ((n + 2) * (n + 1)) / 2;
+  for (let i = 0; i < n; i++) {
+      missingNumber = missingNumber - arr[i];
+     }
+    res.send( { data: missingNumber } );
 })
 
-
-router.get('/test-me2', function (req, res){
-    let arr = [1,2,3,4,6,7]
+router.get("/sol2", function (req, res) {
+  
+    let arr= [33,34, 35, 37, 38];
     let missing=0;
     for(let i=0;i<arr.length;i++){
       if((arr[i+1]-arr[i])!=1){
